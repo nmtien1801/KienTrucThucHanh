06ef0813c6cb03fcecb3830d2c121049b740f323
nmtien1801 <nickname1801@gmail.com>
2025-03-21 21:57:38 +0700

    update tuan3 - Observer

---
+++ tuan3/src/main/java/iuh/fit/Observer.java
@@ -0,0 +1,6 @@
+package iuh.fit;
+
+public interface Observer {
+    public void update();
+    public void setSubject(Subject sub);
+}
---
+++ tuan3/src/main/java/iuh/fit/Subject.java
@@ -0,0 +1,8 @@
+package iuh.fit;
+
+public interface Subject {
+    public void register(Observer o);
+    public void unregister(Observer o);
+    public void notifyObservers();
+    public Object getUpdate(Observer o);
+}
