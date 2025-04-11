b92f8df1fd92068a8178de4293dbd9eafb6c3329
nmtien1801 <nickname1801@gmail.com>
2025-04-11 20:57:02 +0700

    tuan 7

---
+++ Week07_Docker(tt)/NodejsDocker/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+console.log("Server script is starting..."); 
+
+app.get("/", (req, res) => {
+  res.send("Hello, Docker !!!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
